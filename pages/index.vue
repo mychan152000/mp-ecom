dab664a3bd1a0b153258074076289fee4e651d8e
mychan <jmckenzie152000@gmail.com>
2023-08-19 04:20:33 +0700

    add dropdown sort

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,15 @@
     <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
       <LoadingVue v-if="isLoading">Loading...</LoadingVue>
       <div v-else>
+        <div class="my-2">
+          <select v-model="selectedSort" class="form-select">
+            <option v-for="sort in sorts" :value="sort.key" :key="sort.key">
+              {{ sort.label }}
+            </option>
+          </select>
+        </div>
         <div class="grid grid-cols-4 gap-4">
-          <div v-for="product in products" :key="product.id">
+          <div v-for="product in sortedProducts" :key="product.id">
             <ProductCard :product="product" />
           </div>
         </div>
@@ -15,18 +22,33 @@
 
 <script setup>
 import MainLayout from "~/layouts/MainLayout.vue";
-import { ref, onBeforeMount } from "vue";
+import { ref, onBeforeMount, computed } from "vue";
 import { useProductStore } from "@/stores/products";
-import { storeToRefs } from "pinia";
-import { watch } from "vue";
 
 const productStore = useProductStore();
-
 let isLoading = ref(true);
+const sorts = [
+  { key: 1, label: "newest" },
+  { key: 2, label: "price: low to high" },
+];
 
+const selectedSort = ref("");
 const products = computed(() => {
   return productStore.products;
 });
+
+const sortedProducts = computed(() => {
+  if (selectedSort.value === 1) {
+    return [...products.value].sort(
+      (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+    );
+  } else if (selectedSort.value === 2) {
+    return [...products.value].sort((a, b) => a.price - b.price);
+  } else {
+    return products.value;
+  }
+});
+
 onBeforeMount(async () => {
   try {
     const { data } = await useFetch(
